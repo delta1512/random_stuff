195f81f03e19cc73bb789ac8a4df3e5e7e0d22ef
Marcus Belcastro <markbel555@gmail.com>
2016-12-31 14:06:56 +1100

    Added multiple interface support

--- py3bar_modules/Network_usage.py
+++ py3bar_modules/Network_usage.py
@@ -2,19 +2,22 @@
 import commands
 class Py3status:
     def net(self):
-        info = commands.getoutput('ifconfig enp0s3 | grep RX | grep TX')
-        found = False
-        results = []
-        for i, char in enumerate(info):
-            if found or char == ':':
-                if char == ':':
-                    results.append(i+1)
-                found = True
-                if char == ' ':
-                    results.append(i)
-                    found = False
-        RX = float(info[results[0]:results[1]])
-        TX = float(info[results[2]:results[3]])
+        RX, TX = 0, 0
+        interfaces = ['enp0s3'] #Choose interfaces here
+        for x in interfaces:
+            info = commands.getoutput('ifconfig ' + x + ' | grep RX | grep TX')
+            found = False
+            results = []
+            for i, char in enumerate(info):
+                if found or char == ':':
+                    if char == ':':
+                        results.append(i+1)
+                    found = True
+                    if char == ' ':
+                        results.append(i)
+                        found = False
+            RX = RX + float(info[results[0]:results[1]])
+            TX = TX + float(info[results[2]:results[3]])
         results = []
         for data in [RX, TX]:
             if 1000 < data < 1000**2:
@@ -29,5 +32,5 @@ class Py3status:
                 results.append(str(data) + 'B')
         RX = results[0]
         TX = results[1]
-        return {'full_text': '' + RX + ' ' + '' + TX,
+        return {'full_text': '' + str(RX) + ' ' + '' + str(TX),
                 'cached_until': self.py3.time_in(1)}
